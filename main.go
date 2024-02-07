4cb171e26f5c9506a6fbb969db04b43526b5acb6
Igor Glagola <igor@glagola.ru>
2024-02-07 17:25:04 +0000

    test udp server

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("test")
+	server()
+}
+
+func server() {
+	conn, err := net.ListenPacket("udp", ":51820")
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, 1024)
+
+	for {
+		fmt.Println("wait for data")
+		n, addr, err := conn.ReadFrom(buf)
+		if err != nil {
+			panic(err)
+		}
+
+		if n == 0 {
+			continue
+		}
+
+		host, port, err := net.SplitHostPort(addr.String())
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Getting data from %s:%s='%s'\n", host, port, string(buf[:n]))
+	}
 }
